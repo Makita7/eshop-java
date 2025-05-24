c565602387e1630aac40420ac9edd4d0071ae1f7
Sofía Makita <sophiamakita@gmail.com>
2025-05-24 14:56:03 -0300

    adding dto and folders

--- src/main/java/tinta/nube/cafe/eshop/rest/JournalRestController.java
+++ src/main/java/tinta/nube/cafe/eshop/rest/JournalRestController.java
@@ -4,11 +4,15 @@ package tinta.nube.cafe.eshop.rest;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tinta.nube.cafe.eshop.dto.JournalDTO;
 
 @RestController
 @RequestMapping("/journals")
 @CrossOrigin(origins = "http://localhost:9090")
 public interface JournalRestController {
 
+    JournalDTO addJournal(JournalDTO journalItem);
+    void deleteJournal(Long id);
+    JournalDTO updateJournal(JournalDTO journalItem);
 
 }
